61a742671d43cfee88b3e9e46245057fc230dc9f
NaeCatalin <naecatalinvasile@gmail.com>
2025-12-09 14:56:03 +0200

    Add files via upload

---
+++ Exercitiul 7/ex7/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main ()
+            {
+        Console.WriteLine("Introduceti valoarea lui a:");
+        int a = int.Parse(Console.ReadLine());
+        Console.WriteLine("Introduceti valoarea lui b:");
+        int b = int.Parse(Console.ReadLine());
+        // Scimbarea valorilor 
+        int temp = a;
+        a= b;
+        b= temp;
+        Console.WriteLine("Dupa schimbare, valoarea lui a este: " + a);
+        Console.WriteLine("a = " + a);
+        Console.WriteLine("b = " + b);
+    }
+}
