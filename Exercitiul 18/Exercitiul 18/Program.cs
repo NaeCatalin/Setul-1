ecf8a8e672cbb520639d7e46b634d084599c718d
NaeCatalin <naecatalinvasile@gmail.com>
2025-12-09 15:10:49 +0200

    Add files via upload

---
+++ Exercitiul 18/Exercitiul 18/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Citim numarul n de la tastatura
+        Console.WriteLine("Introduceti un numar:");
+        int n = int.Parse(Console.ReadLine());
+
+        int numar = n;
+        int factor = 2;
+
+        Console.Write(n + " = ");
+
+        bool primulFactor = true; // Pentru a nu pune 'x' in fata primului factor
+
+        while (numar > 1)
+        {
+            int putere = 0;
+            while (numar % factor == 0)
+            {
+                numar /= factor;
+                putere++;
+            }
+
+            if (putere > 0)
+            {
+                if (!primulFactor)
+                    Console.Write(" x ");
+                Console.Write(factor + "^" + putere);
+                primulFactor = false;
+            }
+
+            factor++;
+        }
+
+        Console.WriteLine();
+    }
+}
