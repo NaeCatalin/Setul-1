f691582a0381f5673aa7bb968386344a19b6d7b4
NaeCatalin <naecatalinvasile@gmail.com>
2025-12-09 14:57:00 +0200

    Add files via upload

---
+++ Exercitiul 8/Exercitiul_8/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Introduceti valoarea lui a:");
+        int a = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Introduceti valoarea lui b:");
+        int b = int.Parse(Console.ReadLine());
+
+        //Shimbarea fara variabila auxiliara
+        a= a + b;
+        b= a - b;
+        a= a - b;
+
+        Console.WriteLine("Dupa schimbare:");
+        Console.WriteLine("a = " + a);
+        Console.WriteLine("b = " + b);
+    }
+}
