7da3c1abe889d55784caccad43c3e5581316fd39
NaeCatalin <naecatalinvasile@gmail.com>
2025-12-09 14:57:43 +0200

    Add files via upload

---
+++ Exercitiul 9/Exercitiul 9/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Runtime.Serialization.Formatters;
+
+class Program
+{
+    static void Main ()
+    {
+        Console.WriteLine("Introduceti nr n");
+        int n;
+        while(true)
+        {
+            string input = Console.ReadLine();
+            if (int.TryParse(input, out n ))
+            {
+                break;
+            }
+        }
+
+        #region
+        //
+        #endregion
+        Console.WriteLine("Divizorii lui " + n + " sunt:");
+        for (int i = 1; i<= n; i++)
+        {
+            if (n % i == 0)
+            {
+                Console.WriteLine(i + " ");
+            }
+        }
+
+        Console.WriteLine();
+
+    }
+}
