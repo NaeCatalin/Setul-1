551a02436f60065acb80572fc70b984f17e50f48
NaeCatalin <naecatalinvasile@gmail.com>
2025-12-09 14:49:28 +0200

    Add files via upload

---
+++ Exercitiul 4/Exercitiul 4/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Introduceti anul y:");
+        int y = int.Parse(Console.ReadLine());
+        
+        if ((y % 400 == 0) || (y % 4 == 0 && y % 100 != 0))
+        {
+            Console.WriteLine("Anul {0} este an bisect.", y);
+        }
+        else
+        {
+            Console.WriteLine("Anul {0} nu este an bisect.", y);
+        }
+    }
+}
