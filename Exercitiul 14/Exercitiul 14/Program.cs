e2f5a5055ce0129110c6b834cfecd4d6a5af11e5
NaeCatalin <naecatalinvasile@gmail.com>
2025-12-09 15:08:12 +0200

    Add files via upload

---
+++ Exercitiul 14/Exercitiul 14/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Introduceti numarul n: ");
+        int n = int.Parse(Console.ReadLine());
+
+        int original = n;
+        int invers = 0;
+
+        // Tratam numere negative
+        if (n < 0)
+        {
+            Console.WriteLine("Numerele negative NU sunt palindrom.");
+            return;
+        }
+
+        while (n > 0)
+        {
+            int cifra = n % 10;
+            invers = invers * 10 + cifra;
+            n /= 10;
+        }
+
+        if (original == invers)
+            Console.WriteLine("Numarul este palindrom.");
+        else
+            Console.WriteLine("Numarul NU este palindrom.");
+    }
+}
