f2d8ff7fac5929de1d67dfdadcc8b7176de65664
NaeCatalin <naecatalinvasile@gmail.com>
2025-12-09 14:55:36 +0200

    Add files via upload

---
+++ Exercitiul 6/Exercitiul 6/Program.cs
@@ -0,0 +1,39 @@
+﻿//Detreminati daca trei numere pozitive a, b si c pot fi lungimile laturilor unui triunghi. 
+
+using System;
+using System.ComponentModel.Design;
+
+class Program
+
+{
+    static void Main()
+    {
+        Console.Write("Introduceti valoarea lui a: ");
+        double a = double.Parse(Console.ReadLine());
+
+        Console.Write("Introduceti caloarea lui b: ");
+        double b = double.Parse(Console.ReadLine());
+
+        Console.Write("Introduceti valoarea lui c");
+        double c = double.Parse(Console.ReadLine());
+
+        // verificam daca sunt pozitive
+        if (a <= 0 || b <= 0 || c <= 0)
+        {
+            Console.WriteLine("Toate valorile trebuie sa fie pozitive");
+        }
+
+        else
+        {
+            if (a + b > c && b + c > a && c + a > b) 
+            {
+                Console.Write("Numerele pot forma un tringhi");
+            }
+            else
+            {
+                Console.Write("Numerele nu pot forma un triunghi");
+            }
+        }
+    }
+}
+
