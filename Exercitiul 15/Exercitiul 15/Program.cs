a7363cb3c2add77523928a9da4546b628c93d15c
NaeCatalin <naecatalinvasile@gmail.com>
2025-12-09 15:08:44 +0200

    Add files via upload

---
+++ Exercitiul 15/Exercitiul 15/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Introduceti primul numar: ");
+        int a = int.Parse(Console.ReadLine());
+
+        Console.Write("Introduceti al doilea numar: ");
+        int b = int.Parse(Console.ReadLine());
+
+        Console.Write("Introduceti al treilea numar: ");
+        int c = int.Parse(Console.ReadLine());
+
+        int primul, alDoilea, alTreilea;
+
+        // Determinam primul
+        if (a <= b && a <= c)
+            primul = a;
+        else if (b <= a && b <= c)
+            primul = b;
+        else
+            primul = c;
+
+        // Determinam al treilea
+        if (a >= b && a >= c)
+            alTreilea = a;
+        else if (b >= a && b >= c)
+            alTreilea = b;
+        else
+            alTreilea = c;
+
+        // Determinam al doilea
+        alDoilea = a + b + c - primul - alTreilea;
+
+        Console.WriteLine("Numerele in ordine crescatoare: " + primul + " " + alDoilea + " " + alTreilea);
+    }
+}
