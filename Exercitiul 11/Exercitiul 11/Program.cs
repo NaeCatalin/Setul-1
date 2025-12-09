5ee8e6efbac4cf5f191033859ebc6517577fc715
NaeCatalin <naecatalinvasile@gmail.com>
2025-12-09 14:58:38 +0200

    Add files via upload

---
+++ Exercitiul 11/Exercitiul 11/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Introduceti numarul n:");
+        int n = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Cifrele numarului sunt:");
+        if (n == 0)
+        {
+            Console.WriteLine(0);
+            return;
+        }
+        //Separam nr.negative
+        if (n < 0)
+        {
+            Console.WriteLine("-");
+            n = -n;
+        }
+
+        while (n > 0)
+        {
+            int cifra = n % 10;
+            Console.WriteLine(cifra);
+            n /= 10;
+
+        }
+        Console.WriteLine();
+    }
+}
