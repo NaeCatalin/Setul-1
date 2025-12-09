c99fd045025a9750c8abd41400bf89d35fa40082
NaeCatalin <naecatalinvasile@gmail.com>
2025-12-09 14:48:59 +0200

    Add files via upload

---
+++ Exercitiul 3/Exercitiul_3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    #region 3
+    // => qtXaXAniN10YHyxZQJDqCw== <=
+    #endregion
+
+    static void Main()
+    {
+        Console.Write("Introduceti n: ");
+        int n = int.Parse(Console.ReadLine());
+
+        Console.Write("Introduceti k: ");
+        int k = int.Parse(Console.ReadLine());
+
+        if (k == 0)
+        {
+            Console.WriteLine("Impartirea la zero nu este permisa.");
+        }
+        else
+        {
+            if (n % k == 0)
+                Console.WriteLine("n se divide cu k.");
+            else
+                Console.WriteLine("n NU se divide cu k.");
+        }
+    }
+}
