3bcb41fd22750768ecde2a6ca3ec98de521e7052
NaeCatalin <naecatalinvasile@gmail.com>
2025-12-09 14:54:47 +0200

    Add input validation and digit extraction logic

--- Exercitiul 5/Exercitiul 5/Program.cs
+++ Exercitiul 5/Exercitiul 5/Program.cs
@@ -1,1 +1,41 @@
-﻿
+﻿using System;
+
+class Program
+
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Introduceti un numar n: ");
+        string? nInput = Console.ReadLine();
+        if (string.IsNullOrWhiteSpace(nInput))
+        {
+            Console.WriteLine("Valoare introdusa pentru n nu este valida.");
+            return;
+        }
+        long n = long.Parse(nInput);
+
+        Console.Write("Introduceti pozitia k:");
+        string? kInput = Console.ReadLine();
+        if (string.IsNullOrWhiteSpace(kInput))
+        {
+            Console.WriteLine("Valoare introdusa pentru k nu este valida.");
+            return;
+        }
+        int k = int.Parse(kInput);
+        //Lucram cu valoarea absoluta pt a evita probleme nu numerele negative
+        long absN = Math.Abs(n);
+        //Verificam daca nr are cel putin k cifre
+        if (absN < (long)Math.Pow(10, k - 1))
+        {
+            Console.WriteLine($"Numarul {n} nu are {k} cifre.");
+        }
+        else
+        {
+            //Extragem cifra de pe pozitia k
+            long cifraK = (absN / (long)Math.Pow(10, k - 1)) % 10;
+            Console.WriteLine($"A{k}-a cifra a numarului {n} este: {cifraK}");
+        }
+       
+    }
+    
+    }
