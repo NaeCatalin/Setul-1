e09fd0276c9d7b27307ccf6bf6dd149acd12f149
NaeCatalin <naecatalinvasile@gmail.com>
2025-12-09 14:58:10 +0200

    Add files via upload

---
+++ Exercitiul 10/Exercitiul 10/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Introduceti numarul n");
+        int n = int.Parse(Console.ReadLine());
+
+        if (n <2 )
+        {
+            Console.WriteLine("Numarul NU este prim.");
+            return;
+        }
+        bool estePrim = true;
+        for(int i = 2; i*i <= n; i++)
+        {
+            if (n % i ==0)
+            {
+                estePrim = false;
+                break;
+            }
+        }
+        if (estePrim)
+            Console.WriteLine("Numarul este prim");
+        else
+            Console.WriteLine("Numarul NU este prim");
+    }
+}
